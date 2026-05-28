fdf1db2397818a6527d2b45f93ffd11daeb398e0
evtefeev <nikita.evtefeev@gmail.com>
2026-05-28 19:35:47 +0300

    Add functionality to add menu positions and update home page layout
    - Implemented a new route to handle adding menu positions with image upload.
    - Created an HTML form for adding new menu items.
    - Enhanced the home page layout with a grid display for menu items and a button to add new positions.
    - Added a .gitignore file to exclude __pycache__ from version control.

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from os import name
+import os
+import uuid
+
+from flask import Flask, render_template, request
 from database import Menu, Session
 
 
@@ -10,6 +14,42 @@ def index():
     with Session() as cursor:
         positions = cursor.query(Menu).all()
     return render_template("home.html", positions=positions)
+
+
+@app.route("/add_position", methods=["GET", "POST"])
+def add_position():
+    if request.method == "POST":
+        name = request.form.get("name")
+        desc = request.form.get("desc")
+        time = int(request.form.get("time"))
+        cost = int(request.form.get("cost"))
+        image = request.files.get("image")
+
+        # print(name, desc, time, cost, image)
+
+        if not image or not image.filename:
+            return "Файл не вибрано або завантаження не вдалося"
+
+        unique_filename = f"{uuid.uuid4()}_{image.filename}"
+        output_path = os.path.join("static/menu", unique_filename)
+
+        with open(output_path, "wb") as f:
+            f.write(image.read())
+
+        item = Menu(
+            name=name,
+            image=output_path,
+            description=desc,
+            time=time,
+            cost=cost,
+        )
+        with Session() as cursor:
+            cursor.add(item)
+            cursor.commit()
+
+        return "Позиція успішно додана!"
+
+    return render_template("add_position.html")
 
 
 if __name__ == "__main__":
